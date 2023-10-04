be2d8a4088a6acf9d400cb40e92e1a6eb65fb00d
Arthurlmr <arthurlarangeiras@gmail.com>
2023-10-04 16:26:59 -0300

    alterações html e css

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,12 +1,10 @@
-var nome = prompt("Informe seu nome:");
-
 function logar() {
     var usuario = document.getElementById("usuario").value;
     var senha = document.getElementById("senha").value;
     var mensagemElement = document.getElementById("mensagem");
 
-    if (usuario == nome && senha == "1234") {
-        mensagemElement.textContent = "Bem-vindo de volta, " + nome + "!";
+    if (usuario == localStorage.getItem("usuario") && senha == localStorage.getItem("senha")) {
+        mensagemElement.textContent = "Bem-vindo de volta, " + localStorage.getItem("usuario") + "!";
         mensagemElement.style.color = "#00ff88";
         mensagemElement.style.fontSize = "30px";
         
@@ -20,4 +18,14 @@ function logar() {
         mensagemElement.style.color = "red";
         mensagemElement.style.fontSize = "30px";
     }
+}
+
+function cadastrar() {
+    let usuario = document.getElementById("cadastro_usuario").value;
+    let email = document.getElementById("cadastro_email").value;
+    let senha = document.getElementById("cadastro_senha").value;
+
+    localStorage.setItem("usuario", usuario);
+    localStorage.setItem("email", email);
+    localStorage.setItem("senha", senha);
 }
